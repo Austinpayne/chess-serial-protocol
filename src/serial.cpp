d810bfcecb8ff0a8f8315343a97637badcc5784b
Austin Payne <austin.payne.01@gmail.com>
2017-10-29 16:10:46 -0600

    bring serial library over

--- src/serial.cpp
+++ src/serial.cpp
@@ -1,39 +1,83 @@
-/* serial library by Austin Payne
- */
-
+#include "Particle.h"
 #include "serial.h"
+#ifdef SPARK
+#include "serial_spark.h"
+#endif
 
-/**
- * Constructor.
- */
-Serial::Serial()
-{
-  // be sure not to call anything that requires hardware be initialized here, put those in begin()
+// cmd format: `0x[CMD] [params?]\n`
+// or `0x[CMD]\n`
+cmd_f cmds[] = {
+    &do_status,     // FROM motor control
+    &do_new_game,   // FROM motor control
+    &do_end_turn,   // TO   motor control
+    &do_move_piece, // TO   motor control
+    &do_promote,    // FROM motor control
+    &do_calibrate,  // TO   motor control
+    &do_end_game,   // FROM motor control
+    &do_scan_wifi,  // FROM motor control
+    &do_set_wifi    // FROM motor control
+};
+
+int do_serial_command(char *cmd_str) {
+    char *cmd, *params, *tmp;
+    int c;
+    int ret = -1;
+
+    cmd = strtok(cmd_str, " \n");
+    params = strtok(NULL, "\n");
+
+    if (!cmd) {
+#ifdef SPARK
+        Log.error("No command found");
+#endif
+        return ret;
+    }
+#ifdef SPARK
+    Log.info("Got cmd %.4s", cmd);
+#endif
+    c = strtol(cmd, &tmp, 16); // cmd number is in hex
+    if (strcmp(cmd, tmp) == 0) {
+#ifdef SPARK
+        Log.error("Command bits are invalid");
+#endif
+        return ret;
+    }
+#ifdef SPARK
+    Log.info("cmd number: %d", c);
+#endif
+    if (c < sizeof(cmds) && cmds[c]) {
+        ret = cmds[c](params); // params may be NULL
+    } else {
+#ifdef SPARK
+        Log.warn("Serial cmd %d not implemented", c);
+#endif
+    }
+
+    return ret;
 }
 
-/**
- * Example method.
- */
-void Serial::begin()
-{
-    // initialize hardware
-    Serial.println("called begin");
-}
+void rx_serial_command(char c) {
+    #define SERIAL_BUFF_SIZE 64
 
-/**
- * Example method.
- */
-void Serial::process()
-{
-    // do something useful
-    Serial.println("called process");
-    doit();
-}
+    static char rx_buffer[SERIAL_BUFF_SIZE];
+    static int i = 0;
 
-/**
-* Example private method
-*/
-void Serial::doit()
-{
-    Serial.println("called doit");
+    if (i < SERIAL_BUFF_SIZE) {
+        if (c == '\r' || c == '\n') {
+            rx_buffer[i] = '\0';
+            if (do_serial_command(rx_buffer) == 0) {
+#ifdef SPARK
+                Log.trace("Serial cmd complete");
+#endif
+            } else {
+#ifdef SPARK
+                Log.error("Serial cmd failed");
+#endif
+            }
+            CLEAR_BUFF(rx_buffer, SERIAL_BUFF_SIZE, i);
+        } else {
+            rx_buffer[i] = c;
+            i++;
+        }
+    }
 }
--- src/serial.h
+++ src/serial.h
@@ -1,33 +1,41 @@
-#pragma once
+#ifndef __SERIAL_H_
+#define __SERIAL_H_
 
-/* serial library by Austin Payne
- */
+#define CMD_FMT  "%01x"
+#define MOVE_FMT "%.4s"
+#define CMD_STATUS     0x0
+#define CMD_NEW_GAME   0x1
+#define CMD_END_TURN   0x2
+#define CMD_MOVE_PIECE 0x3
+#define CMD_PROMOTE    0x4
+#define CMD_CALIBRATE  0x5
+#define CMD_END_GAME   0x6
+#define SCAN_WIFI      0x7
+#define SET_WIFI       0x8
 
-// This will load the definition for common Particle variable types
-#include "Particle.h"
+#define OK 0
 
-// This is your main class that users will import into their application
-class Serial
-{
-public:
-  /**
-   * Constructor
-   */
-  Serial();
+#define CLEAR_BUFF(b,s,i)  memset(b, 0, s); i = 0
+// use SEND_CMD_P in other macros, PF is format specifier
+#ifdef SPARK
+#define SEND_CMD(c)        Serial1.printlnf(CMD_FMT, (c))
+#define SEND_CMD_P(c,PF,p) Serial1.printlnf(CMD_FMT " " PF, (c), (p))
+#else // assume stm32
+#define SEND_CMD(c)        printf(CMD_FMT "\n", (c))
+#define SEND_CMD_P(c,PF,p) printf(CMD_FMT " " PF "\n", (c), (p))
+#endif
+#define SEND_MOVE(m)       SEND_CMD_P(CMD_MOVE_PIECE, MOVE_FMT, (m))
 
-  /**
-   * Example method
-   */
-  void begin();
+typedef int (*cmd_f)(char *);
 
-  /**
-   * Example method
-   */
-  void process();
+int do_status(char *params);
+int do_new_game(char *params);
+int do_end_turn(char *params);
+int do_promote(char *params);
+int do_end_game(char *params);
+int do_scan_wifi(char *params);
+int do_set_wifi(char *params);
+int do_serial_command(char *cmd_str);
+void rx_serial_command(char c);
 
-private:
-  /**
-   * Example private method
-   */
-  void doit();
-};
+#endif /* __SERIAL_H_ */
